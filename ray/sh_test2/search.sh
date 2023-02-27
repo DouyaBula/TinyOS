2a4120baa54eadbb246fa6142daa702f4d25e14c
Doxel <douyashadow@163.com>
2023-02-27 21:35:56 +0800

    first try

--- ray/sh_test2/search.sh
+++ ray/sh_test2/search.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 #First you can use grep (-n) to find the number of lines of string.
 #Then you can use awk to separate the answer.
+grep -n $2 $1 > temp
+awk -F: '1 {print $1}' temp > $3
+rm temp
