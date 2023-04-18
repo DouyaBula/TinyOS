d17f2af45dbe6d710a58913f763c2fc6008614fd
Doxel <douyashadow@163.com>
2023-04-18 20:20:03 +0800

    bug fix

--- kern/sched.c
+++ kern/sched.c
@@ -36,23 +36,15 @@ void schedule(int yield) {
 	 *   'TAILQ_FIRST', 'TAILQ_REMOVE', 'TAILQ_INSERT_TAIL'
 	 */
 	/* Exercise 3.12: Your code here. */
-    #define DEBUG 0
     if (yield != 0 || count == 0 || e == NULL || e->env_status != ENV_RUNNABLE) {
         if (e != NULL && e->env_status == ENV_RUNNABLE) {
+            TAILQ_REMOVE(&env_sched_list, e, env_sched_link);
             TAILQ_INSERT_TAIL(&env_sched_list, e, env_sched_link);
         }
         if (TAILQ_EMPTY(&env_sched_list)) {
             panic("schedule: no runnable envs");
         }
         e = TAILQ_FIRST(&env_sched_list);
-        if (DEBUG) {
-            if (e->env_status == ENV_RUNNABLE) {
-                printk(">>> It is runnable.\n");
-            } else {
-                printk(">>> It is NOT runnable!\n");
-            }
-        }    
-        TAILQ_REMOVE(&env_sched_list, e, env_sched_link);
         count = e->env_pri;
     }
     count--;
